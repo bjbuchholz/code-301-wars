dcb49a8ef3564a6cdfd33777b7d4cd62c3f3e0f1
Brandon Buchholz <blue.buchholz@gmail.com>
2017-12-09 15:55:28 -0800

    completed kata, passes all test

---
+++ insert-dashes/solution.js
@@ -0,0 +1,14 @@
+function insertDash(num) {
+  var inStr = String(num);
+  var outStr = inStr[0], i;
+
+  for (i = 1; i < inStr.length; i++) {
+    if (inStr[i - 1] % 2 !== 0 && inStr[i] % 2 !== 0) {
+      outStr += '-';
+    }
+
+    outStr += inStr[i];
+  }
+
+  return outStr;
+}
